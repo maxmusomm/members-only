13f1274c78069fc6e53907a2eaf4c591b52c6c2c
maxmusomm <maxmusomm@gmail.com>
2025-03-14 02:12:06 +0200

    changed the query functions to include a public. for the supabase db

--- db/query.js
+++ db/query.js
@@ -1,29 +1,29 @@
 const pool = require('./pool');
 
 const getAllUsers = async () => {
-    const { rows } = await pool.query('SELECT * FROM users');
+    const { rows } = await pool.query('SELECT * FROM public.users');
     return rows;
 }
 
 const getUserById = async (_id) => {
-    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [_id]);
+    const { rows } = await pool.query('SELECT * FROM public.users WHERE id = $1', [_id]);
     return rows[0];
 }
 
 const createUser = async (name, password) => {
-    await pool.query('INSERT INTO users (name, password) VALUES ($1, $2)', [name, password]);
+    await pool.query('INSERT INTO public.users (name, password) VALUES ($1, $2)', [name, password]);
 }
 
 const createMessage = async (title, content, user_id) => {
-    await pool.query('INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)', [title, content, user_id]);
+    await pool.query('INSERT INTO public.messages (title, content, user_id) VALUES ($1, $2, $3)', [title, content, user_id]);
 }
 const getAllMessages = async () => {
-    const { rows } = await pool.query('SELECT * FROM messages');
+    const { rows } = await pool.query('SELECT * FROM public.messages');
     return rows;
 }
 
 const getUserByName = async (name) => {
-    const { rows } = await pool.query('SELECT * FROM users WHERE name = $1', [name]);
+    const { rows } = await pool.query('SELECT * FROM public.users WHERE name = $1', [name]);
     return rows[0];
 }
 
